860ff5bdcacc6ab0966eb76c2e942814379a3e32
cathy810218 <cathy810218@gmail.com>
2017-07-13 15:27:22 -0700

    Fix keyboard layout

--- QuickFill/KeyboardViewController.m
+++ QuickFill/KeyboardViewController.m
@@ -20,10 +20,43 @@
 @end
 
 @implementation KeyboardViewController
+//
+//- (void)updateViewConstraints {
+//    [super updateViewConstraints];
+//}
 
-- (void)updateViewConstraints {
-    [super updateViewConstraints];
-    // Add custom view sizing constraints here
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.myKeyboard = [[QuickFill alloc]init];
+    self.myKeyboard = [[[NSBundle mainBundle] loadNibNamed:@"QuickFill" owner:self options:nil] objectAtIndex:0];
+    self.inputView = (UIInputView *)self.myKeyboard;
+
+//    self.myKeyboard.bounds = CGRectMake(0, 0, 350, 200);
+    self.myKeyboard.nextKeyboard.layer.cornerRadius = 5.0;
+    self.myKeyboard.nextKeyboard.layer.masksToBounds = false;
+    self.myKeyboard.nextKeyboard.layer.shadowOpacity = 1.0;
+    self.myKeyboard.nextKeyboard.layer.shadowRadius = 0;
+    self.myKeyboard.nextKeyboard.layer.shadowOffset = CGSizeMake(0, 1.0);
+    self.myKeyboard.nextKeyboard.layer.shadowColor = [UIColor grayColor].CGColor;
+    
+    [self setKeyboardKey:self.myKeyboard.city withKey: NSLocalizedString(@"city",nil)];
+    [self setKeyboardKey:self.myKeyboard.state withKey: NSLocalizedString(@"state",nil)];
+    [self setKeyboardKey:self.myKeyboard.zip withKey: NSLocalizedString(@"zip",nil)];
+    [self setKeyboardKey:self.myKeyboard.email withKey: NSLocalizedString(@"email",nil)];
+    [self setKeyboardKey:self.myKeyboard.address1 withKey: NSLocalizedString(@"address",nil)];
+    [self setKeyboardKey:self.myKeyboard.phoneNum withKey: NSLocalizedString(@"phoneNum",nil)];
+    [self setKeyboardKey:self.myKeyboard.firstName withKey: NSLocalizedString(@"firstName",nil)];
+    [self setKeyboardKey:self.myKeyboard.lastName withKey: NSLocalizedString(@"lastName",nil)];
+    [self setKeyboardKey:self.myKeyboard.spaceKey withKey: NSLocalizedString(@"space",nil)];
+    [self setKeyboardKey:self.myKeyboard.returnKey withKey: NSLocalizedString(@"return",nil)];
+    [self setKeyboardKey:self.myKeyboard.deleteKey withKey: NSLocalizedString(@"delete",nil)];
+    [self setKeyboardKey:self.myKeyboard.optionKey withKey: NSLocalizedString(@"optional", nil)];
+    
+    [self addGestureToKeyboard];
+    
+    UIImage *globalImg = [UIImage imageNamed:@"gkey.png"];
+    [self.myKeyboard.nextKeyboard setImage:globalImg forState:UIControlStateNormal];
+    [self.inputView addSubview:self.myKeyboard.nextKeyboard];
 }
 
 - (void)setKeyboardKey:(UIButton *)btn withKey:(NSString *)name{
@@ -46,47 +79,6 @@
     }
 }
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    self.myKeyboard = [[QuickFill alloc]init];
-    self.myKeyboard = [[[NSBundle mainBundle] loadNibNamed:@"QuickFill" owner:self options:nil] objectAtIndex:0];
-    self.inputView = (UIInputView *)self.myKeyboard;
-
-    self.myKeyboard.bounds = CGRectMake(0, 0, 350, 200);
-    self.myKeyboard.nextKeyboard.layer.cornerRadius = 5.0;
-    self.myKeyboard.nextKeyboard.layer.masksToBounds = false;
-    self.myKeyboard.nextKeyboard.layer.shadowOpacity = 1.0;
-    self.myKeyboard.nextKeyboard.layer.shadowRadius = 0;
-    self.myKeyboard.nextKeyboard.layer.shadowOffset = CGSizeMake(0, 1.0);
-    self.myKeyboard.nextKeyboard.layer.shadowColor = [UIColor grayColor].CGColor;
-    
-    [self setKeyboardKey:self.myKeyboard.city withKey: NSLocalizedString(@"city",nil)];
-    [self setKeyboardKey:self.myKeyboard.state withKey: NSLocalizedString(@"state",nil)];
-    [self setKeyboardKey:self.myKeyboard.zip withKey: NSLocalizedString(@"zip",nil)];
-    [self setKeyboardKey:self.myKeyboard.email withKey: NSLocalizedString(@"email",nil)];
-    [self setKeyboardKey:self.myKeyboard.address1 withKey: NSLocalizedString(@"address",nil)];
-    [self setKeyboardKey:self.myKeyboard.phoneNum withKey: NSLocalizedString(@"phoneNum",nil)];
-    [self setKeyboardKey:self.myKeyboard.firstName withKey: NSLocalizedString(@"firstName",nil)];
-    [self setKeyboardKey:self.myKeyboard.lastName withKey: NSLocalizedString(@"lastName",nil)];
-    [self setKeyboardKey:self.myKeyboard.spaceKey withKey: NSLocalizedString(@"space",nil)];
-    [self setKeyboardKey:self.myKeyboard.returnKey withKey: NSLocalizedString(@"return",nil)];
-    [self setKeyboardKey:self.myKeyboard.deleteKey withKey: NSLocalizedString(@"delete",nil)];
-    [self setKeyboardKey:self.myKeyboard.optionKey withKey: NSLocalizedString(@"optional", nil)];
-
-    
-    [self addGestureToKeyboard];
-    
-    UIImage *globalImg = [UIImage imageNamed:@"gkey.png"];
-    [self.myKeyboard.nextKeyboard setImage:globalImg forState:UIControlStateNormal];
-    [self.inputView addSubview:self.myKeyboard.nextKeyboard];
-    
-    NSUserDefaults *shared = [[NSUserDefaults alloc] initWithSuiteName:@"group.com.cathyoun.QuickFillKeyboard"];
-    id userInfo = [shared valueForKey:@"userInfo"];
-    NSLog(@"%@",userInfo[@"firstName"]);
-    
-    
-}
-
 - (void)textWillChange:(id<UITextInput>)textInput {
     // The app is about to change the document's contents. Perform any preparation here.
 }
@@ -100,59 +92,76 @@
     } else {
         textColor = [UIColor blackColor];
     }
-    //  [self.nextKeyboardButton setTitleColor:textColor forState:UIControlStateNormal];
 }
 
-#pragma Mark Keyboards
+#pragma mark - Keyboards
 - (void)addGestureToKeyboard {
     
-    [self.myKeyboard.nextKeyboard addTarget:self action:@selector(advanceToNextInputMode) forControlEvents:UIControlEventTouchUpInside];
+    [self.myKeyboard.nextKeyboard addTarget:self
+                                     action:@selector(advanceToNextInputMode)
+                           forControlEvents:UIControlEventTouchUpInside];
     
-    [self.myKeyboard.spaceKey addTarget:self action:@selector(pressSpaceKey) forControlEvents:UIControlEventTouchUpInside];
+    [self.myKeyboard.spaceKey addTarget:self
+                                 action:@selector(pressSpaceKey)
+                       forControlEvents:UIControlEventTouchUpInside];
     
-    [self.myKeyboard.returnKey addTarget:self action:@selector(pressReturnKey) forControlEvents:UIControlEventTouchUpInside];
+    [self.myKeyboard.returnKey addTarget:self
+                                  action:@selector(pressReturnKey)
+                        forControlEvents:UIControlEventTouchUpInside];
     
-    [self.myKeyboard.deleteKey addTarget:self action:@selector(touchBegin:) forControlEvents:UIControlEventTouchDown];
-    [self.myKeyboard.deleteKey addTarget:self action:@selector(touchEnd:) forControlEvents:
-     UIControlEventTouchUpInside | UIControlEventTouchUpOutside | UIControlEventTouchCancel];
+    [self.myKeyboard.deleteKey addTarget:self
+                                  action:@selector(touchBegin:)
+                        forControlEvents:UIControlEventTouchDown];
+    [self.myKeyboard.deleteKey addTarget:self
+                                  action:@selector(touchEnd:)
+                        forControlEvents: UIControlEventTouchCancel];
     
-    [self.myKeyboard.firstName addTarget:self action:@selector(pressFirstNameKey) forControlEvents:UIControlEventTouchUpInside];
+    [self.myKeyboard.firstName addTarget:self
+                                  action:@selector(pressFirstNameKey)
+                        forControlEvents:UIControlEventTouchUpInside];
     
-    [self.myKeyboard.lastName addTarget:self action:@selector(pressLastNameKey) forControlEvents:UIControlEventTouchUpInside];
+    [self.myKeyboard.lastName addTarget:self
+                                 action:@selector(pressLastNameKey)
+                       forControlEvents:UIControlEventTouchUpInside];
     
-    [self.myKeyboard.email addTarget:self action:@selector(pressEmailKey) forControlEvents:UIControlEventTouchUpInside];
+    [self.myKeyboard.email addTarget:self
+                              action:@selector(pressEmailKey)
+                    forControlEvents:UIControlEventTouchUpInside];
     
-    [self.myKeyboard.phoneNum addTarget:self action:@selector(pressPhoneKey) forControlEvents:UIControlEventTouchUpInside];
+    [self.myKeyboard.phoneNum addTarget:self
+                                 action:@selector(pressPhoneKey)
+                       forControlEvents:UIControlEventTouchUpInside];
     
-    [self.myKeyboard.address1 addTarget:self action:@selector(pressAddress1Key) forControlEvents:UIControlEventTouchUpInside];
+    [self.myKeyboard.address1 addTarget:self
+                                 action:@selector(pressAddress1Key)
+                       forControlEvents:UIControlEventTouchUpInside];
     
-    [self.myKeyboard.optionKey addTarget:self action:@selector(pressOptionKey) forControlEvents:UIControlEventTouchUpInside];
+    [self.myKeyboard.optionKey addTarget:self
+                                  action:@selector(pressOptionKey)
+                        forControlEvents:UIControlEventTouchUpInside];
     
-    [self.myKeyboard.city addTarget:self action:@selector(pressCityKey) forControlEvents:UIControlEventTouchUpInside];
+    [self.myKeyboard.city addTarget:self
+                             action:@selector(pressCityKey)
+                   forControlEvents:UIControlEventTouchUpInside];
     
-    [self.myKeyboard.state addTarget:self action:@selector(pressStateKey) forControlEvents:UIControlEventTouchUpInside];
+    [self.myKeyboard.state addTarget:self
+                              action:@selector(pressStateKey)
+                    forControlEvents:UIControlEventTouchUpInside];
     
-    [self.myKeyboard.zip addTarget:self action:@selector(pressZipKey) forControlEvents:UIControlEventTouchUpInside];
+    [self.myKeyboard.zip addTarget:self
+                            action:@selector(pressZipKey)
+                  forControlEvents:UIControlEventTouchUpInside];
     
     
 }
-
-- (void)pressDeleteKey {
-    if (self.timeCount < 8) {
-        [self.textDocumentProxy deleteBackward];
-    } else  {
-        while ([self.textDocumentProxy documentContextBeforeInput].length > 0) {
-            for (int i = 0; i < self.textDocumentProxy.documentContextBeforeInput.length; i++){
-                [self.textDocumentProxy deleteBackward];
-            }
-        }
-    }
-}
-
 
 - (void)touchBegin:(UIButton*)sender {
     // set a timer
-    self.timer = [NSTimer scheduledTimerWithTimeInterval:0.06 target:self selector:@selector(increaseTimer) userInfo:nil repeats:YES];
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:0.06
+                                                  target:self
+                                                selector:@selector(increaseTimer)
+                                                userInfo:nil
+                                                 repeats:YES];
 }
 
 - (void)touchEnd:(UIButton*)sender {
@@ -164,10 +173,19 @@
 - (void)increaseTimer {
     self.timeCount++;
     [self pressDeleteKey];
-    
 }
 
-
+- (void)pressDeleteKey {
+    if (self.timeCount < 6) {
+        [self.textDocumentProxy deleteBackward];
+    } else  {
+        while ([self.textDocumentProxy documentContextBeforeInput].length > 0) {
+            for (int i = 0; i < self.textDocumentProxy.documentContextBeforeInput.length; i++){
+                [self.textDocumentProxy deleteBackward];
+            }
+        }
+    }
+}
 
 - (void)pressSpaceKey {
     [self.textDocumentProxy insertText:@" "];
